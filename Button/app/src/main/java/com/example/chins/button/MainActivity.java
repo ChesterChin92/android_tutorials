23e7d157a96651e7c04c131388858063dc1d484c
ChesterChin92 <chinschian@hotmail.com>
2016-02-02 01:13:48 +0800

    Initial commit

---
+++ Button/app/src/main/java/com/example/chins/button/MainActivity.java
@@ -0,0 +1,90 @@
+package com.example.chins.button;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+
+        //Declaration and  onClickListener must be setup in onCreate
+        Button btn = (Button) findViewById(R.id.button);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+
+                Toast.makeText(getApplicationContext(), "hello world", Toast.LENGTH_LONG).show();
+            }
+        });
+
+//        Similar to the concept int varname = (int) someinput
+//        Prevent parsing wrong type
+        ImageButton btn2 = (ImageButton) findViewById(R.id.button2);
+
+        btn2.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(),"hello world from image",Toast.LENGTH_SHORT).show(); }
+        });
+
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+//    Simple implementation,
+//    In XML must include click1 in the Android:onclick properties
+//    Button btn = (Button) findViewById(R.id.button);
+//    Button button= (Button) findViewById(R.id.button);
+//
+//
+//    public void click1(View v) {
+//        Toast.makeText(this,"hello world",Toast.LENGTH_SHORT).show();
+//    }
+
+
+
+
+}
